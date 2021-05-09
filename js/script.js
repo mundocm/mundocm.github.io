69d1463927f6042f0b7499828580abeb1f0802b6
Gabriel Carvalho Silva <gabriel_carvalho@usp.br>
2021-05-09 16:33:10 -0300

    First files from old repo

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.querySelector(".overlayMenu").style.display = "block";
+  }
+  
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.querySelector(".overlayMenu").style.display = "none";
+}
